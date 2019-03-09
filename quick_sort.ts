2f0a15aaba5443ffb79c936ab7985e2ae79fcc73
MinuteWang <243560619@qq.com>
2019-03-09 23:56:07 +0800

    qucik sort

---
+++ quick_sort.ts
@@ -0,0 +1,11 @@
+const quickSort = (list: number[]): number[] => {
+  if (list.length <= 1) return list;
+  const basePoint = list[0];
+  const leftList = [];
+  const rightList = [];
+  for (let i = 1; i < list.length; i++) {
+    const item = list[i];
+    item >= basePoint ? rightList.push(item) : leftList.push(item);
+  }
+  return quickSort(leftList).concat(quickSort(rightList));
+};
